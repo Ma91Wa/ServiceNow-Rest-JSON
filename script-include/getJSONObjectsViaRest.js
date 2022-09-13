cdace078d3020f6bbefc78b90319335d0d645f38
Ma91Wa <92052704+Ma91Wa@users.noreply.github.com>
2022-09-13 07:37:46 +0200

    Update getJSONObjectsViaRest.js
    add outer_keyname as parameter

--- script-include/getJSONObjectsViaRest.js
+++ script-include/getJSONObjectsViaRest.js
@@ -1,4 +1,4 @@
-function getJSONObjectsViaRest(endpoint, objects, api, request) {
+function getJSONObjectsViaRest(endpoint, objects, api, request, outer_keyname) {
     var pagedR = new sn_ws.RESTMessageV2(api, request);
     if (endpoint !== null) {
         pagedR.setEndpoint(endpoint);
@@ -7,9 +7,9 @@ function getJSONObjectsViaRest(endpoint, objects, api, request) {
     var pagedResponseBody = pagedResponse.getBody();
     var pagedhttpStatus = pagedResponse.getStatusCode();
     var pagedObj = JSON.parse(pagedResponseBody);
-    objects = objects.concat(pagedObj.value);
+    objects = objects.concat(pagedObj[outer_keyname]);
     if (pagedObj["@odata.nextLink"]) {
-        objects = getJSONObjectsViaRest(pagedObj["@odata.nextLink"], objects, api, request);
+        objects = getJSONObjectsViaRest(pagedObj["@odata.nextLink"], objects, api, request, outer_keyname);
     }
     return objects;
 }
