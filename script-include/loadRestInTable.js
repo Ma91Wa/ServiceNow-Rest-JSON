8b16932a08de836ed59f95de271d53d659fac39b
Ma91Wa <92052704+Ma91Wa@users.noreply.github.com>
2022-09-13 07:37:15 +0200

    Update loadRestInTable.js
    add outer_keyname as parameter

--- script-include/loadRestInTable.js
+++ script-include/loadRestInTable.js
@@ -1,12 +1,12 @@
-function loadRestInTable(rest_message, http_method, target_table) {
+function loadRestInTable(rest_message, http_method, target_table, outer_keyname) {
     try {
-        var objects = getJSONObjectsViaRest(null, [], rest_message, http_method);
+        var objects = getJSONObjectsViaRest(null, [], rest_message, http_method, outer_keyname);
 
         for (var i=0;  i<objects.length; i++ ) {
             parseJSONObject(objects[i], target_table);
         }
     } catch (ex) {
         var message = ex.message;
-        gs.info('JSON IMPORT ERROR: ' + message);
+        gs.info('JSON IMPORT ERROR: ' + rest_message + ' (' + http_method + '): ' + message);
     }
 }
